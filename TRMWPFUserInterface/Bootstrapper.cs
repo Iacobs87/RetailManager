dd647de7111c8fbc43e7c61f06fab24505155271
Alexandru Iacoboae <alexiacoboae@gmail.com>
2020-09-15 09:59:23 +0300

    Wire Up LogIn Form to API
    Connects the Log In form button to the authentication API endpoint(/token). Gets back the bearer token or an exception if failed

--- TRMWPFUserInterface/Bootstrapper.cs
+++ TRMWPFUserInterface/Bootstrapper.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using TRMWPFUserInterface.Helpers;
 using TRMWPFUserInterface.ViewModels;
 
 namespace TRMWPFUserInterface
@@ -16,6 +18,12 @@ namespace TRMWPFUserInterface
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
+
         }
 
         protected override void Configure()
@@ -24,7 +32,8 @@ namespace TRMWPFUserInterface
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
